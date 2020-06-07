a36ee976f74f4eee75222372084e8679ef1a63e3
Felipe <felipem@take.net>
2020-06-07 01:32:20 -0300

    create base node.js project

--- api/src/server.ts
+++ api/src/server.ts
@@ -4,11 +4,16 @@ const app = express();
 
 app.get("/users", (req, res) => {
   console.log("teste");
-  res.send("ok");
+  res.json(["Felipe", "Felipe"]);
 });
+
+app.listen(3000);
 
 //npm install express
 //npm install @type/express -d
 //npm install ts-node -d
 //npm install typescript -d
 //npx tsc --init
+//npm install ts-node-dev -D
+//npx ts-node-dev ./src/server.ts
+//add script dev in package.json
