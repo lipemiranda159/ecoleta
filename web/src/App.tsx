0d7b80a444bde2069fe5b7bc4d34a727f866d9a5
Felipe <felipem@take.net>
2020-06-07 10:24:05 -0300

    create header component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
 
+import Header from "./header";
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <div>
+      <Header />
+      <h1>Conteúdo da aplicação</h1>
+    </div>
+  );
 }
 
 export default App;
