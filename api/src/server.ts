9e4e6b79c9ab2266f3a394885b877dc09a202683
Felipe <felipem@take.net>
2020-06-07 01:44:18 -0300

    remove steps from server.ts

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,13 +5,3 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3000);
-
-//npm install express
-//npm install @type/express -d
-//npm install ts-node -d
-//npm install typescript -d
-//npx tsc --init
-//npm install ts-node-dev -D
-//npx ts-node-dev ./src/server.ts
-//add script dev in package.json
-//npx create-react-app web --template=typescript
