a96a428761de4dbbe02d4027c4cb596c6039229e
Felipe <felipem@take.net>
2020-06-07 09:58:09 -0300

    config cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from "express";
 import routes from "./routes";
 import path from "path";
+import cors from "cors";
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
