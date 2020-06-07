6e7447a87111eacee190f73cfe22f2bed86538fe
Felipe <felipem@take.net>
2020-06-07 02:58:13 -0300

    add images, create static content route

--- api/src/database/seeds/create_items.ts
+++ api/src/database/seeds/create_items.ts
@@ -1,8 +1,8 @@
 import Knex from "knex";
 export async function seed(knex: Knex) {
   await knex("items").insert([
-    { title: "Lâmpadas", image: "lampada.svg" },
-    { title: "Pilhas e baterias", image: "bateria.svg" },
+    { title: "Lâmpadas", image: "lampadas.svg" },
+    { title: "Pilhas e baterias", image: "baterias.svg" },
     { title: "Papéis e papelão", image: "papeis-papelao.svg" },
     { title: "Resíduos Eletrônicos", image: "eletronicos.svg" },
     { title: "Resíduos Orgânicos", image: "organicos.svg" },
