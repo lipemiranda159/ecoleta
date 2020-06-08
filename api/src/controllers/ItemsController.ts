5b96d8927818d1b9e703fd4c331a684bc64abb3f
Felipe <felipem@take.net>
2020-06-08 00:23:04 -0300

    return id item in items controller

--- api/src/controllers/ItemsController.ts
+++ api/src/controllers/ItemsController.ts
@@ -6,6 +6,7 @@ class ItemsController {
     const items = await knex("items").select("*");
     const serializedItems = items.map((item) => {
       return {
+        id: item.id,
         title: item.title,
         image_url: `http://localhost:3000/uploads/${item.image}`,
       };
